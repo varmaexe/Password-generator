49a2149b9138345def85c328de0b6cffa539e6e1
sai verma <vectorvarma0303@gmail.com>
2023-09-13 23:26:17 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const inputSlider = document.querySelector("[data-lengthSlider]");
+const lengthDisplay = document.querySelector("[data-lengthNumber]")
+const passwordDisplay = document.querySelector("[data-passwordDisplay]")
+const copyBtn = document.querySelector("[data-copy]");
+const copyMsg = document.querySelector("[data-copyMsg]");
+const uppercaseCheck = document.querySelector("[#uppercase]");
+const lowercaseCheck = document.querySelector("[#lowercase]");
+const numbersCheck = document.querySelector("[#numbers]");
+const symbolsCheck = document.querySelector("[#symbols]");
+const indicator = document.querySelector("[#data-indicator]");
+const generateBtn = document.querySelector(".generateButton");
+const allCheckBox = document.querySelectorAll("input[type=checkbox]")
+
+let password = "";
+let passwordLength = 10;
+let checkCount = 1;
+// set strength circle color to grey
